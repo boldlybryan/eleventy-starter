2eb1653071cefa8fab126efc2129e90bb48dcb65
Bryan King <bpking15@gmail.com>
2019-06-09 17:09:47 -0400

    added layout type for pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
 module.exports = function(eleventyConfig){
+
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
+
 }
