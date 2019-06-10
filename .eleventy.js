ac520cc8c23ccb9187fa1c69bfffe75395323a27
Bryan King <bpking15@gmail.com>
2019-06-09 20:59:04 -0400

    Add filter so humans can read the dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig){
 
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
   eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
+
+  eleventyConfig.addFilter("readableDate", (dateObj, format = "LLLL d, y") => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc"
+    }).toFormat(format);
+  });
 
 }
